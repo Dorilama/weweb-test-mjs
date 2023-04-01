4131493235a65134192495029a251b763e5e7d68
dorilama <assioma.xi@gmail.com>
2023-04-01 19:46:04 +0200

    initial commit

---
+++ src/wwElement.vue
@@ -0,0 +1,32 @@
+<template>
+	<div class="my-element">
+		<p :style="textStyle">I am a custom element !</p>
+	</div>
+</template>
+
+<script>
+import { useBluetooth } from "@vueuse/core";
+
+useBluetooth();
+
+export default {
+	props: {
+		content: { type: Object, required: true },
+	},
+	computed: {
+		textStyle() {
+			return {
+				color: this.content.textColor,
+			};
+		},
+	},
+};
+</script>
+
+<style lang="scss" scoped>
+.my-element {
+	p {
+		font-size: 18px;
+	}
+}
+</style>
